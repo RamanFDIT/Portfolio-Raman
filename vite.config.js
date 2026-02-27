50c9ca053d270e7c2e3fb490ece8c736b3229836
Raman <ramandeep.singh31@student.kpu.ca>
2026-02-27 12:03:58 -0800

    SEO

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import prerender from 'vite-plugin-prerender'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    prerender({
+      routes: [
+        '/',
+        '/about',
+        '/resume',
+        '/contact',
+        '/projects/dashboard',
+        '/projects/edith',
+        '/projects/ascend',
+      ],
+    }),
+  ],
 })
